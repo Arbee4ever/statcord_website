8c3909ae4086eebbb14c5bac3f461977e405671a
Arbee4ever <robin.baenecke@mail.de>
2022-09-09 13:47:33 +0200

    testing usercards

---
+++ src/routes/[serverId]/[userId]/usercard/+page@usercard.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	import MemberCard from '$components/MemberCard.svelte';
+	import { onMount } from 'svelte';
+	let json: any;
+
+	onMount(async () => {
+		let body = { serverId: $page.params.serverId, userId: $page.params.userId };
+		console.log(JSON.stringify(body));
+		let discorddata = await fetch('https://statcord-data.arbee.workers.dev', {
+			method: 'POST',
+			body: JSON.stringify(body)
+		});
+		json = await discorddata.json();
+		json.sort(function (a: any, b: any) {
+			return b.score - a.score;
+		});
+	});
+</script>
+
+<main>
+	<div class="card">
+		{#if json}
+			{#if json.length == 0}
+				<div class="loading">
+					<p>Statcord is not on this Server!</p>
+					<a id="addToDiscord">Not yet available</a>
+				</div>
+			{:else}
+				{#each json as { name, pfp, score }, i}
+					<MemberCard {pfp} {name} {score} {i} />
+				{/each}
+			{/if}
+		{:else}
+			<p class="loading">Please wait, data is loading...</p>
+		{/if}
+	</div>
+</main>
+
+<style>
+	@import url(https://fonts.bunny.net/css?family=josefin-sans:400);
+
+	.card {
+		position: relative;
+		height: fit-content;
+		background: #1a1a1a99;
+		border-radius: 10px;
+		background: rgba(0, 0, 0, 0.25);
+		box-shadow: 0 0 32px 0 rgba(0, 0, 0, 0.37);
+		height: min-content;
+		padding: 2vh;
+	}
+
+	.loading {
+		text-align: center;
+		vertical-align: middle;
+		margin: 1vh;
+	}
+
+	#addToDiscord {
+		margin-top: 1vh;
+		border-top-right-radius: 10px;
+		border-top-left-radius: 10px;
+		height: 4vh;
+		align-content: center;
+		background: rgba(0, 0, 0, 0.25);
+		box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+		border-radius: 10px;
+		text-align: center;
+		grid-area: 1 / 1 / 2 / 2;
+		color: gray;
+		cursor: default;
+		height: 4vh;
+		line-height: 4vh;
+		padding: 1vh;
+	}
+</style>
