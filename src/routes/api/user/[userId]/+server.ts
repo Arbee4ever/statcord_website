fa82e00f6d21ccfdad6c9ab00a3b4647d5051c50
Arbee4ever <arbee@arbeeco.de>
2022-09-12 09:21:23 +0200

    change leaderboard styling and loading behaviour

---
+++ src/routes/api/user/[userId]/+server.ts
@@ -0,0 +1,34 @@
+import { env } from '$env/dynamic/private';
+import { error } from '@sveltejs/kit';
+
+/** @type {import('../../../../../.svelte-kit/types/src/routes/api/guild/[serverId]/$types').RequestHandler} */
+export async function GET({ params }) {
+    if (params.userId.length != 18) {
+        throw error(400, "Invalid User ID: " + params.userId);
+    }
+    const discordData = await getDiscordData(params.userId);
+    const discordDataJson = await discordData.json();
+    const jsonElement = {
+        "name": discordDataJson.username + "#" + discordDataJson.discriminator,
+        "pfp": "https://cdn.discordapp.com/avatars/" + params.userId + "/" + discordDataJson.avatar,
+    };
+    return new Response(
+        JSON.stringify(jsonElement), {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS'
+        }
+    });
+}
+
+/**
+ * @param {string} id
+ */
+async function getDiscordData(id: string) {
+    return await fetch('https://discord.com/api/users/' + id, {
+        method: 'GET',
+        headers: {
+            Authorization: 'Bot ' + env.DISCORD_AUTH
+        }
+    });
+}
