67fe4c293d7e1c51687f2c3b96a0e9662f17d8b6
Arbee4ever <arbee@arbeeco.de>
2022-10-26 17:35:07 +0200

    make dashboard accessible by anyone

--- src/routes/dashboards/[guildId]/+page.server.ts
+++ src/routes/dashboards/[guildId]/+page.server.ts
@@ -2,24 +2,24 @@ import { error, redirect } from "@sveltejs/kit";
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ cookies, params }) {
-    const userReq = await fetch(`https://discordapp.com/api/users/@me`, {
-        headers: { 'Authorization': `Bearer ${cookies.get('disco_access_token')}` }
-    });
+    // const userReq = await fetch(`https://discordapp.com/api/users/@me`, {
+    //     headers: { 'Authorization': `Bearer ${cookies.get('disco_access_token')}` }
+    // });
 
-    if (userReq.status != 200) {
-        throw error(userReq.status, userReq.statusText)
-    }
+    // if (userReq.status != 200) {
+    //     throw error(userReq.status, userReq.statusText)
+    // }
 
-    const user = await userReq.json();
+    // const user = await userReq.json();
 
-    const guildReq = await fetch('https://api.statcord.arbeeco.de/guilds/' + params.guildId + "?user=" + user.id);
-    if (guildReq.status != 200) {
-        throw redirect(302, "/");
-    }
-    const guildResp = await guildReq.json();
-    if (!guildResp.moderator) {
-        throw redirect(302, "/");
-    }
+    // const guildReq = await fetch('https://api.statcord.arbeeco.de/guilds/' + params.guildId + "?user=" + user.id);
+    // if (guildReq.status != 200) {
+    //     throw redirect(302, "/");
+    // }
+    // const guildResp = await guildReq.json();
+    // if (!guildResp.moderator) {
+    //     throw redirect(302, "/");
+    // }
 
     const configReq = await fetch('https://api.statcord.arbeeco.de/guilds/' + params.guildId + "/config");
     if (configReq.status != 200) {
@@ -28,7 +28,6 @@ export async function load({ cookies, params }) {
     const configResp = configReq.json();
 
     return {
-        user: user,
         config: configResp
     }
 }
