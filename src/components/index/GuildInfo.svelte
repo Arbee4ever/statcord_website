6e6ce7ead30361bc55c98788aa8a6c78c1b7afeb
Arbee4ever <arbee@arbeeco.de>
2023-02-02 13:25:54 +0100

    - fix ad causing weird layout on mobile

--- src/components/index/GuildInfo.svelte
+++ src/components/index/GuildInfo.svelte
@@ -101,6 +101,26 @@
 	{/if}
 </div>
 
+<div class="adsbygoogle">
+	<script
+		async
+		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1252158636066695"
+		crossorigin="anonymous"
+	></script>
+	<!-- Under Guildinfo -->
+	<ins
+		class="adsbygoogle"
+		style="display:block"
+		data-ad-client="ca-pub-1252158636066695"
+		data-ad-slot="5674378409"
+		data-ad-format="auto"
+		data-full-width-responsive="true"
+	/>
+	<script>
+		(adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
+</div>
+
 <style lang="scss">
 	.guildInfo {
 		display: flex;
@@ -145,6 +165,10 @@
 		}
 
 		.infoOther {
+			display: none;
+		}
+
+		.adsbygoogle {
 			display: none;
 		}
 	}
