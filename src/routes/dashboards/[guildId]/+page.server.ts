2eba9fae80e4f970c6b4f051c3a40476b83397bf
Arbee4ever <arbee@arbeeco.de>
2024-10-27 00:48:21 +0200

    fix: improve routing to main dashboard site

--- src/routes/dashboards/[guildId]/+page.server.ts
+++ src/routes/dashboards/[guildId]/+page.server.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
-	throw redirect(302, `${params.guildId}/values`);
+	throw redirect(302, `/dashboards/${params.guildId}/values`);
 }
